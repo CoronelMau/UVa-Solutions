b1ead0d552f02df2f2c7fb166fcf8794b5ad3af2
Mau <8mau.coronel@gmail.com>
2024-10-01 22:46:24 -0600

    UVa 12643 - Tennis Rounds solution

---
+++ 12643 - Tennis Rounds.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int n, i, j;
+
+  while (cin >> n >> i >> j)
+  {
+    int round = 0;
+
+    while (i != j)
+    {
+      i = (i + 1) / 2;
+      j = (j + 1) / 2;
+      round++;
+    }
+
+    cout << round << endl;
+  }
+
+  return 0;
+}
